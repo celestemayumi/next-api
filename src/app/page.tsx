4e7e5a285a5f2c6cd8ee72b085af191a88ff40ea
Celeste Tanaka <cehmayumi@gmail.com>
2024-04-18 09:41:17 -0300

    feat: renderizar dados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const RES = await fetch('http://localhost:3000/api/clients')
+  const data = await RES.json()
+
+console.log(data)
+
   return (
     <main>
-      Content
+      {
+        data.map((item:any) => {
+          return (
+            <>
+            <div className="border bg-gray-400 m-3 p-2 flex justify-center">
+              <div className="uppercase flex-1">{item.name}</div>
+              <div className="italic flex-1">{item.age}</div>
+              <div className="italic flex-1">{item.address}</div>
+            </div>
+            </>
+          )
+        }) 
+      }
     </main>
   );
-}
+} 
